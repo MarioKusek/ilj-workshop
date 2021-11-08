e1fb2cb0dfa897cc765a4fb54526561f53278dd2
MarioKusek <mario.kusek@fer.hr>
2021-11-08 08:13:19 +0100

    HtmlGenerator can generate list of file infos

--- files/src/test/java/hr/fer/ilj/workshop/files/HtmlGeneratorTest.java
+++ files/src/test/java/hr/fer/ilj/workshop/files/HtmlGeneratorTest.java
@@ -20,14 +20,44 @@ class HtmlGeneratorTest {
 
   @Test
   void generateHtmlForOneElement() {
-    HtmlGenerator generator = new HtmlGenerator();
+    HtmlGenerator generator = new HtmlGenerator() {
+      @Override
+      public String toHtml(FileInfo fileInfo) {
+        return fileInfo.name();
+      }
+    };
     List<FileInfo> list = List.of(new FileInfo("ime", Path.of("dir/ime"), 0, FileType.FILE));
+
     assertThat(generator.generate(list)).isEqualTo("""
             <p>File list:</p>
             <ul>
               <li>ime</li>
             </ul>
             """);
+  }
+
+  @Test
+  void generateHtmlForManyElement() {
+    HtmlGenerator generator = new HtmlGenerator() {
+      @Override
+      public String toHtml(FileInfo fileInfo) {
+        return fileInfo.name();
+      }
+    };
+    List<FileInfo> list = List.of(
+        new FileInfo("ime1", Path.of("dir/ime1"), 0, FileType.FILE),
+        new FileInfo("ime2", Path.of("dir/ime2"), 0, FileType.FILE),
+        new FileInfo("ime3", Path.of("dir/ime3"), 0, FileType.FILE)
+    );
+
+    assertThat(generator.generate(list)).isEqualTo("""
+        <p>File list:</p>
+        <ul>
+          <li>ime1</li>
+          <li>ime2</li>
+          <li>ime3</li>
+        </ul>
+        """);
   }
 
 }
