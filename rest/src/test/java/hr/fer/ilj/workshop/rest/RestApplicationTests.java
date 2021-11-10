3862ea10b08d13f4d17179fefe9ff4418c547bc9
MarioKusek <mario.kusek@fer.hr>
2021-11-10 06:03:31 +0100

    Create rest project with Spring Boot

---
+++ rest/src/test/java/hr/fer/ilj/workshop/rest/RestApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.fer.ilj.workshop.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
